4103472fb34b194e5e536e4d1253d1525e57e2ed
Danielle Navarro <d.navarro@unsw.edu.au>
2019-03-11 14:39:28 +1100

    moved lightness to separate function

--- R/asciify.R
+++ R/asciify.R
@@ -1,12 +1,30 @@
+
+# function to compute the lightness of a character
+# called internally by ascii_map, but not exported
+#' @importFrom graphics text
+#' @importFrom imager implot
+#' @importFrom imager imfill
+#' @importFrom imager grayscale
+lightness <- function(chr) {
+  imager::implot(
+    imager::imfill(50, 50, val = 1), 
+    graphics::text(x = 25, y = 25, labels = chr, cex = 5)
+  ) %>% 
+  imager::grayscale() %>% 
+  mean()
+}
+
+
 #' Creates ASCII art from an image
 #'
-#' @return Something
+#' @param file A character string specifying the path to the file
+#' @param charset A character string that lists the set of characters to use
+#' @param threshold Lightness value at which to truncate
+#' @return A data frame with...
 #' @examples
 #' print("hi")
 #' @importFrom imager load.image
 #' @importFrom imager as.cimg
-#' @importFrom imager implot
-#' @importFrom imager imfill
 #' @importFrom imager grayscale
 #' @importFrom imager imresize
 #' @importFrom dplyr %>%
@@ -15,24 +33,14 @@
 #' @importFrom purrr map_dbl
 #' @importFrom ggplot2 cut_number
 #' @export
-asciify <- function(file, charset, threshold){
+ascii_map <- function(file, charset = c(LETTERS, letters), threshold = .5){
   
   # load image
   im <- imager::load.image(file) 
   im <- imager::as.cimg(im[,,1:3])
   
-  # function to compute the lightness of a character
-  greyval <- function(chr) {
-    imager::implot(
-      imager::imfill(50,50,val=1), 
-      text(25,25,chr,cex=5)
-    ) %>% 
-    imager::grayscale() %>% 
-    mean()
-  }
-  
   # compute lightness of all characters
-  g <- purrr::map_dbl(charset, greyval)
+  g <- purrr::map_dbl(.x = charset, .f = lightness)
   
   # sort and count
   charset <- charset[order(g)]
@@ -45,7 +53,7 @@ asciify <- function(file, charset, threshold){
     as.data.frame() %>%                    # convert to tibble
     dplyr::filter(value < threshold) %>%   # threshold the image
     dplyr::mutate(
-      qv = ggplot2cut_number(value, n) %>% as.integer(), # discretise
+      qv = ggplot2::cut_number(value, n) %>% as.integer(), # discretise
       char = charset[qv]  # map to a character
     )
   
